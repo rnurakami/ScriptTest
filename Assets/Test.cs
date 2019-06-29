123d75f8d1dc2e576e4d23cd5c6a4d5526a9d528
rnurakami <mariamanzini8@gmail.com>
2019-06-29 10:28:26 +0900

    Add Boss Class to Test.cs
    Boss can  use magic attack.

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -23,12 +23,54 @@ public class Test : MonoBehaviour {
         {
             Debug.Log(array[i]);
         }
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+        //Magic関数を10回使った後に、更にMagic関数を呼び出すと、mpが足りないメッセージが表示されることを確認してください
+        Boss lastboss = new Boss();
+        for (int i=0; i<11; i++)
+        {
+            lastboss.Magic();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
 
+public class Boss
+{
+    private int hp = 100; // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53; // 魔力
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 魔法攻撃用の関数
+    public void Magic()
+    {
+        if (mp < 5)
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+            return;
+        }
+
+        mp -= 5;
+
+        Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+}
